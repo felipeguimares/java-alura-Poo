1011b0a7be519cd420163f530894d7d177d61d55
Felipe Guimarães <141097410+felipeguimares@users.noreply.github.com>
2024-06-26 10:47:15 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alo Mundo!");
+        System.out.println("Bem vindo ao ScreenWatch");
+        int anoDeLancamento = 2022;
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        String sinopse = """
+                         Filme: Top Gun: Maverick
+                         Estrelado por Tom Cruise
+                         Muito Bom!
+                         Ano de Lançamento: 
+                         """ +anoDeLancamento;
+        System.out.println(sinopse);
+        int classificacao = (int) (media/2);
+        System.out.println(classificacao);
+
+
     }
 }
 
