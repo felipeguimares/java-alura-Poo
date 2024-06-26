b8af6a60c93257848b30bfc9809d9b873b9ac9f4
Felipe Guimarães <felipeguimares.dev@gmail.com>
2024-06-25 23:42:23 -0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alo Mundo!");
+        System.out.println("Bem vindo ao ScreenMatch");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        //Validação para informar se está na assinatura ou não o filme
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
+        // Média calculada do filme
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println("Nota do filme " + media);
+        String sinopse;
+        sinopse = """
+                Filme de aventura
+                Estrelado por Tom Cruise
+                Ano de Lançamento: """ + anoDeLancamento;
+        System.out.println(sinopse);
+
+
+
     }
 }
+
 
 
 
