c2ff9f01b678e1422c0bbb3d5ad2ad960617a6c4
Felipe Guimarães <141097410+felipeguimares@users.noreply.github.com>
2024-06-26 12:31:28 -0300

    update 26/06

--- src/Loop.java
+++ src/Loop.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Loop {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite seu filme");
-        String filme =  leitura.nextLine();
-        System.out.println("Qual o ano de lançamento");
-        int anoDeLancamento = leitura.nextInt();
+
+        for (int i = 0; i < ; i++) {
+            
+        }
         System.out.println("Qual a nota você dá para esse filme");
         double nota = leitura.nextDouble();
     }
