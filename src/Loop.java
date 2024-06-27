0518b4e9a35d77cfb68cb67d3794604ac69ddacc
Felipe Guimarães <141097410+felipeguimares@users.noreply.github.com>
2024-06-27 10:50:27 -0300

    A

--- src/Loop.java
+++ src/Loop.java
@@ -3,11 +3,15 @@ import java.util.Scanner;
 public class Loop {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
-
-        for (int i = 0; i < ; i++) {
-            
+        double mediaAvaliacao = 0;
+        double nota;
+        for (int i = 0; i < 3 ; i++) {
+            System.out.println("Qual a nota você dá para esse filme");
+            nota = leitura.nextDouble();
+            mediaAvaliacao += nota;
         }
-        System.out.println("Qual a nota você dá para esse filme");
-        double nota = leitura.nextDouble();
+        System.out.println(String.format("Media de avaliação %.2f",mediaAvaliacao/3));
+
+
     }
 }
