ae33ac0740da0ea9c3ac8c30d5f340ccfa64a50e
Felipe Guimarães <felipeguimares.dev@gmail.com>
2024-06-29 00:56:38 -0300

    DesafioFinal
    desafio do curso introdutório a java na alura feito por mim

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
@@ -6,28 +7,30 @@ public class Main {
         double valorRecebido = 0;
         double valorTransferido = 0;
         int operacoes = 0;
-        System.out.println("*******************************");
-        System.out.println("Dados inciais do cliente");
-        System.out.println("");
-        System.out.println("Nome: Felipe Guimarães");
-        System.out.println("Tipo de Conta: Corrente");
-        System.out.println(String.format("Saldo Inicial : %.2f", saldoInicial));
-        System.out.println("*******************************");
-        System.out.println("Operações");
-        System.out.println("");
-        System.out.println("1 - Consultar saldos");
-        System.out.println("2 - Receber Valor");
-        System.out.println("3 - Transferir Valor");
-        System.out.println("4 - Sair");
-        System.out.println("");
 
-        while (operacoes == 0) {
+        boolean continuar = true;
+        while (continuar) {
+            System.out.println("*******************************");
+            System.out.println("Dados inciais do cliente");
+            System.out.println("");
+            System.out.println("Nome: Felipe Guimarães");
+            System.out.println("Tipo de Conta: Corrente");
+            System.out.println(String.format("Saldo Inicial : %.2f", saldoInicial));
+            System.out.println("*******************************");
+            System.out.println("Operações");
+            System.out.println("");
+            System.out.println("1 - Consultar saldos");
+            System.out.println("2 - Receber Valor");
+            System.out.println("3 - Transferir Valor");
+            System.out.println("4 - Sair");
+            System.out.println("");
+
             System.out.println("Digite a opção desejada");
             operacoes = leitor.nextInt();
             switch (operacoes) {
                 case 1:
                     System.out.println(String.format("Seu saldo é %.2f", saldoInicial));
-                    operacoes = 0;
+                    break;
                 case 2:
                     System.out.println("Qual o valor recebido?");
                     valorRecebido = leitor.nextDouble();
@@ -37,16 +40,21 @@ public class Main {
                 case 3:
                     System.out.println("Qual o valor que deseja transferir: ");
                     valorTransferido = leitor.nextDouble();
-                    saldoInicial -= valorTransferido;
-                    System.out.println(String.format("Saldo atualizado: %.2f", saldoInicial));
+                    if (valorTransferido > saldoInicial) {
+                        System.out.println("Não há saldo suficiente para fazer essa transferência.");
+                    } else {
+                        saldoInicial -= valorTransferido;
+                        System.out.println(String.format("Saldo atualizado: %.2f", saldoInicial));
+                    }
                     break;
                 case 4:
                     System.out.println("Até mais!");
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida");
                     break;
             }
         }
     }
 }
-
-
-
